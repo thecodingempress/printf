93d05732b0f6342f9f345de2d206fdd456188c6c
codyempress <codyempress@gmail.com>
2023-11-14 22:14:26 +0300

    edits

--- main.h
+++ main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+
 int _printf(const char *format, ...);
 int _putchar(char w);
 char *inttostr(int num, char *str);
-
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent();
 #endif
--- printf.c
+++ printf.c
@@ -1,58 +1,41 @@
 #include "main.h"
 #include <string.h>
 #include <stdarg.h>
-/**
- * _printf - prints output according to format
- * @format: character string with format
- *
- * Return: number of chars printed
- */
+
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent();
 
 int _printf(const char *format, ...)
 {
 	size_t i = 0;
 	size_t j = 0;
-	size_t k = 0;
-	char *s;
 
 	va_list(args);
-
 	if (!format)
 	{
 		return (-1);
 	}
 	va_start(args, format);
-	
 	while (format && format[i])
-	{
-		if (format[i] == '%')
+	{	if (format[i] == '%')
 		{
 			i++;
-			if (format[i] == 'c')
+			switch (format[i])
 			{
-				_putchar(va_arg(args, int));
-				j++;
-			}
-			else if (format[i] == 's')
-			{
-				s = va_arg(args, char*);
-				
-				for (k = 0; s[k]; k++)
-				{
-					_putchar(s[k]);
-					j++;
-				}
-			}
-			else if (format[i] == '%')
-			{
-				_putchar('%');
-				j++;
-			}
-			else
-			{
-				_putchar('%');
-				_putchar(format[i]);
-				j += 2;
+				case 'c':
+					j += print_char(args);
+				       break;
+				case 's':
+				       j += print_string(args);
+				      break;
+				case '%':
+      				       j += print_percent();
+				       break;
+				default:
+				       _putchar('%');
+				       _putchar(format[i]);
+				       j += 2;
 			}
 		}
 		else
@@ -62,7 +45,6 @@ int _printf(const char *format, ...)
 		}
 		i++;
 	}
-
 	va_end(args);
 	return (j);
 }
